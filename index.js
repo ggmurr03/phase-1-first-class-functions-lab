63e9bc1323f5ebfcc56708acf4a9d2f948bb2aed
Gregory Murray <gr.j.murray@gmail.com>
2024-10-29 02:22:17 -0700

    added advanced first class functions

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (array){
+      return array.slice(0,2)
+}
+const returnLastTwoDrivers = function (array) {
+    return array.slice(array.length-2)
+}
+function createFareMultiplier(int){return function(multiply)
+    {return int*multiply}}
+
+    
+const fareDoubler = function (int){return createFareMultiplier(int)(2)}
+const fareTripler = function (int){return createFareMultiplier(int)(3)}
+function selectDifferentDrivers(arrayOfDrivers, funct) {
+    if (funct === returnFirstTwoDrivers){
+        return arrayOfDrivers.slice(0,2)}
+    else if (funct === returnLastTwoDrivers){
+        return arrayOfDrivers.slice(length-2)}
+    }
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+   
